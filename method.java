dee6807ec2249657bcc9deaf1cfc5fba3f9fcf6b
PremKumar Mistry <120615589+premkumarmistry@users.noreply.github.com>
2022-12-17 05:31:40 +0530

    Add files via upload

---
+++ method.java
@@ -0,0 +1,17 @@
+
+
+public class method {
+    public void methodDemo(int a ){
+            System.out.println("This is Method Example");
+
+    }
+
+
+        public static void main(String[] args) {
+            method obj = new method();
+            obj.methodDemo(12);
+        }
+
+}
+// OUTPUT--->>>
+// This is Method Example
